c38b172b3a0c4646894ba173365348d0bf22b3c1
AshDashTooFast3 <338127@student.mboutrecht.nl>
2026-01-10 14:11:36 +0100

    titel aangepast

--- resources/views/leveranciers/edit.blade.php
+++ resources/views/leveranciers/edit.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leverancier Details</title>
+    <title>Leverancier Wijzigen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
